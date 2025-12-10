c4fb29a225a03506b7ecd8f9de9bb1ee19e8b19e
Baryonics <benedikt.leicher@gmail.com>
2025-12-11 00:40:21 +0100

    Added a crude implementation of data to compact converters.

--- backend/srs/converters/StructSerializer.cpp
+++ backend/srs/converters/StructSerializer.cpp
@@ -1,13 +1,47 @@
 #include "StructSerializer.hpp"
 #include "srs/converters/DataConverterBase.hpp"
 #include "srs/data/SRSDataStructs.hpp"
+#include "srs/utils/CommonAlias.hpp"
+#include "srs/utils/CommonDefinitions.hpp"
+#include "srs/data/SRSDataCompact.hpp"
+#include "srs/utils/CommonFunctions.hpp"
+#include <algorithm>
+#include <bitset>
+#include <boost/asio/any_io_executor.hpp>
 #include <cstddef>
+#include <cstdint>
 #include <expected>
+#include <spdlog/spdlog.h>
+#include <stdexcept>
 #include <string_view>
 #include <vector>
+#include <zpp_bits.h>
 
 namespace srs::process
 {
+    namespace
+    {
+        void marker_to_compact(const MarkerData& marker_data, internal::MarkerDataCompact& marker_data_compact)
+        {
+            // needs common::split_bits to handle timestamp
+            marker_data_compact.flag = static_cast<decltype(marker_data_compact.flag)>(0);
+            marker_data_compact.vmm_id = static_cast<decltype(marker_data_compact.vmm_id)>(marker_data.vmm_id);
+        }
+
+        void hit_to_compact(const HitData& hit_data, internal::HitDataCompact& hit_data_compact)
+        {
+            hit_data_compact.vmm_id = static_cast<decltype(hit_data_compact.vmm_id)>(hit_data.vmm_id);
+            hit_data_compact.flag = static_cast<decltype(hit_data_compact.flag)>(1);
+            hit_data_compact.adc = static_cast<decltype(hit_data_compact.adc)>(hit_data.adc);
+            hit_data_compact.bc_id = static_cast<decltype(hit_data_compact.bc_id)>(hit_data.bc_id);
+            // hit_data_compact.bc_id = common::bin_to_grey(hit_data_compact.bc_id);
+            hit_data_compact.channel_num = static_cast<decltype(hit_data_compact.channel_num)>(hit_data.channel_num);
+            hit_data_compact.is_over_threshold = static_cast<decltype(hit_data_compact.is_over_threshold)>(hit_data.is_over_threshold);
+            hit_data_compact.offset = static_cast<decltype(hit_data_compact.offset)>(hit_data.offset);
+
+        }
+    }; // namespace
+
     StructSerializer::StructSerializer(size_t n_lines)
         : ConverterTask{ "Struct deserializer", none, n_lines }
     {
@@ -18,9 +52,8 @@ namespace srs::process
     auto StructSerializer::convert([[maybe_unused]] const StructData* input, [[maybe_unused]] std::vector<char>& output)
         -> std::expected<std::size_t, std::string_view>
     {
-        // NOTE: DO NOT do any memory allocation here. If heap data is needed, please initialize it in the constructor.
-        output.clear();
-        return std::unexpected{ "not implemented" };
+        // auto serialize_to = zpp::bits::out{ output, zpp::bits::endian::network{}, zpp::bits::no_size{} };
+        return 0;
     }
     // NOLINTEND
 } // namespace srs::process
