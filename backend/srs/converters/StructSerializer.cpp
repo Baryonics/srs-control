49abe60a5035d11eb52407d8bf0c3b7593dd49cd
Baryonics <benedikt.leicher@gmail.com>
2025-12-11 13:40:27 +0100

    compact converters should work now

--- backend/srs/converters/StructSerializer.cpp
+++ backend/srs/converters/StructSerializer.cpp
@@ -23,7 +23,10 @@ namespace srs::process
     {
         void marker_to_compact(const MarkerData& marker_data, internal::MarkerDataCompact& marker_data_compact)
         {
-            // needs common::split_bits to handle timestamp
+            auto timestamp_bits = std::bitset<common::SRS_TIMESTAMP_HIGH_BIT_LENGTH + common::SRS_TIMESTAMP_LOW_BIT_LENGTH>(marker_data.srs_timestamp);
+            auto timestamp_low_bits = std::bitset<common::SRS_TIMESTAMP_LOW_BIT_LENGTH>{};
+            auto timestamp_high_bits = std::bitset<common::SRS_TIMESTAMP_HIGH_BIT_LENGTH>{};
+            common::split_bits(timestamp_bits, timestamp_high_bits, timestamp_low_bits);
             marker_data_compact.flag = static_cast<decltype(marker_data_compact.flag)>(0);
             marker_data_compact.vmm_id = static_cast<decltype(marker_data_compact.vmm_id)>(marker_data.vmm_id);
         }
@@ -34,11 +37,10 @@ namespace srs::process
             hit_data_compact.flag = static_cast<decltype(hit_data_compact.flag)>(1);
             hit_data_compact.adc = static_cast<decltype(hit_data_compact.adc)>(hit_data.adc);
             hit_data_compact.bc_id = static_cast<decltype(hit_data_compact.bc_id)>(hit_data.bc_id);
-            // hit_data_compact.bc_id = common::bin_to_grey(hit_data_compact.bc_id);
+            hit_data_compact.bc_id = common::bin_to_gray(hit_data_compact.bc_id);
             hit_data_compact.channel_num = static_cast<decltype(hit_data_compact.channel_num)>(hit_data.channel_num);
             hit_data_compact.is_over_threshold = static_cast<decltype(hit_data_compact.is_over_threshold)>(hit_data.is_over_threshold);
             hit_data_compact.offset = static_cast<decltype(hit_data_compact.offset)>(hit_data.offset);
-
         }
     }; // namespace
 
@@ -52,7 +54,7 @@ namespace srs::process
     auto StructSerializer::convert([[maybe_unused]] const StructData* input, [[maybe_unused]] std::vector<char>& output)
         -> std::expected<std::size_t, std::string_view>
     {
-        // auto serialize_to = zpp::bits::out{ output, zpp::bits::endian::network{}, zpp::bits::no_size{} };
+        auto serialize_to = zpp::bits::out{ output, zpp::bits::endian::network{}, zpp::bits::no_size{} };
         return 0;
     }
     // NOLINTEND
